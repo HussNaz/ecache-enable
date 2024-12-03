8c976cafe3b5f9c98ff65a14b553ec6ca58efa7f
HussNaz <nazmulhassanc09@gmail.com>
2024-12-03 17:16:02 +0600

    implement liquibase

---
+++ src/main/java/com/webbfontaine/training/license/managament/entity/Inspector.java
@@ -0,0 +1,32 @@
+package com.webbfontaine.training.license.managament.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import lombok.Data;
+
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+@Data
+@Entity
+@Table(name = "INSPECTOR", schema = "LICENSE_USER")
+public class Inspector {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "INSPECTOR_ID", nullable = false)
+    @JsonIgnore
+    private Long inspectorId;
+
+    @Column(name = "NAME")
+    private String name;
+
+    @Column(name = "PHONE")
+    private String phone;
+
+    @Column(name = "EMAIL")
+    private String email;
+
+}
